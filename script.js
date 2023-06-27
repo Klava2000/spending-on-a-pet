557511515c9d8f5d4dc0f98418d24c7d40a8cfd3
klava_lit <klava_lit@mail.ru>
2023-06-27 15:42:36 +0300

    cycles js

--- script.js
+++ script.js
@@ -3,14 +3,34 @@ button.addEventListener("click", calculateAmount);
 
 function calculateAmount(e) {
     e.preventDefault();
-    const food = document.querySelector("#food").value;
-    const filler = document.querySelector("#filler").value;
-    const vet = document.querySelector("#vet").value;
-    const groom = document.querySelector("#groom").value;
-    const yummy = document.querySelector("#yummy").value;
-    const toy = document.querySelector("#toy").value;
+    const income = +document.querySelector('#income').value; 
 
-    let totalSum = +food + +filler + +vet + +groom + +yummy + +toy;
+    let totalExpenses = 0;
+    const expenses = document.querySelectorAll('.expenses');
 
-    document.querySelector("#totalSum").textContent = "  " + totalSum;
+    for (let i = 0; i < expenses.length; i++) {
+        if(expenses[i].value === '') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Пожалуйста, заполните все поля!',
+                text: 'Если значение отсутствует, укажите 0'     
+            })
+            return false;
+        } else {
+            totalExpenses += +expenses[i].value; 
+        }
+    }
+
+    const remainder = income - totalExpenses;
+
+    if(income === 0 || income === ''){
+        Swal.fire({
+            icon: 'error',
+            title: 'Сожалеем, но...',
+            text: 'у Вас нет доходов'     
+            })
+        }
+
+    document.querySelector("#remainder").textContent = remainder;
+    document.querySelector("#totalSum").textContent = totalExpenses;
 }
